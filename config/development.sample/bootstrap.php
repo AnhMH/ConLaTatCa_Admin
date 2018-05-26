0da62da777e24c93a50cb19d570f59c368b3c56d
AnhMH <anhmh@lifull-tech.vn>
2018-05-27 00:57:21 +0700

    Modify config sample

--- config/development.sample/bootstrap.php
+++ config/development.sample/bootstrap.php
@@ -5,12 +5,12 @@
 
 use Cake\Core\Configure;
 
-define('USE_SUB_DIRECTORY', '/baroque/admin');
+define('USE_SUB_DIRECTORY', '');
 
-Configure::write('API.Host', 'http://api.baroque.localhost/');
+Configure::write('API.Host', 'http://api.conlatatca.localhost/');
 Configure::write('Config.HTTPS', false);
 
 Configure::write('Config.CKeditor', array(
     'basel_dir'=>'path/upload/',
-    'basel_url'=>'http://img.baroque.dev/'
+    'basel_url'=>'http://img.conlatatca.localhost/'
 ));
